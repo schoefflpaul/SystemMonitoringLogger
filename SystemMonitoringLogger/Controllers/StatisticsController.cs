b6db13f4c1fe55c4f43059cab513b112a381db9e
Paul Schöffl <pauli.schoeffl@gmail.com>
2020-03-17 14:15:18 +0100

    Added function GetStatisticsForDevice

--- SystemMonitoringLogger/Controllers/StatisticsController.cs
+++ SystemMonitoringLogger/Controllers/StatisticsController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SystemMonitoringLogger.Data;
+using SystemMonitoringLogger.Entities;
 using SystemMonitoringLogger.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace SystemMonitoringLogger.Controllers
 {
@@ -20,6 +22,17 @@ namespace SystemMonitoringLogger.Controllers
         {
             _context = context;
             _mqttService = mqttService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<SystemInfo[]> GetStatisticsForDevice(string id)
+        {
+            return await _context.SystemInfo
+                .Include(e => e.Cpu)
+                .Include(r => r.Ram)
+                .Where(sys => sys.Id == id)
+                .ToArrayAsync();
         }
     }
 }
